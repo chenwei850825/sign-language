45e91ed2a2ff53d30b60ee2399621f30d769fb34
ycw <chenwei850825@gmail.com>
2020-06-21 12:58:02 +0800

    1.fix livedemo to win10 runnable 2. store label file for class reading

--- livedemo.py
+++ livedemo.py
@@ -34,7 +34,7 @@ from model_mobile import features_2D_load_model
 from model_lstm import lstm_load
 from model_i3d import I3D_load
 from predict import probability2label
-
+import pickle
 
 def livedemo():
 	
@@ -51,15 +51,16 @@ def livedemo():
 	# files
 	sClassFile       = "data-set/%s/%03d/class.csv"%(diVideoSet["sName"], diVideoSet["nClasses"])
 	sVideoDir        = "data-set/%s/%03d"%(diVideoSet["sName"], diVideoSet["nClasses"])
-	
+
 	print("\nStarting gesture recognition live demo ... ")
 	print(os.getcwd())
 	print(diVideoSet)
-	
+
 	# load label description
 	#oClasses = VideoClasses(sClassFile)
 
-	sModelFile = "model/20200619-0535-tsl063-oflow-i3d-entire-best.h5"
+	#sModelFile = "model/20200619-0535-tsl063-oflow-i3d-entire-best.h5"
+	sModelFile = "model/20180627-0729-chalearn020-oflow-i3d-entire-best.h5"
 	h, w = 224, 224
 	keI3D = I3D_load(sModelFile, diVideoSet["nFramesNorm"], (h, w, 2), 63)
 
@@ -70,55 +71,67 @@ def livedemo():
 	nCount = 0
 	sResults = ""
 	timer = Timer()
+	nTop = 3
 
 	# loop over action states
-	while True:
-		# show live video and wait for key stroke
-		key = video_show(oStream, "green", "Press <blank> to start", sResults, tuRectangle = (h, w))
-		
-		# start!
-		if key == ord(' '):
-			# countdown n sec
-			video_show(oStream, "orange", "Recording starts in ", tuRectangle = (h, w), nCountdown = 3)
+	with open ('label', 'rb') as fp:
+		label = pickle.load(fp)
+		while True:
+			# show live video and wait for key stroke
+			key = video_show(oStream, "green", "Press <blank> to start", sResults, tuRectangle = (h, w))
 			
-			# record video for n sec
-			fElapsed, arFrames, _ = video_capture(oStream, "red", "Recording ", \
-				tuRectangle = (h, w), nTimeDuration = int(diVideoSet["fDurationAvg"]), bOpticalFlow = False)
-			print("\nCaptured video: %.1f sec, %s, %.1f fps" % \
-				(fElapsed, str(arFrames.shape), len(arFrames)/fElapsed))
+			# start!
+			if key == ord(' '):
+				# countdown n sec
+				video_show(oStream, "orange", "Recording starts in ", tuRectangle = (h, w), nCountdown = 3)
+				
+				# record video for n sec
+				fElapsed, arFrames, _ = video_capture(oStream, "red", "Recording ", \
+					tuRectangle = (h, w), nTimeDuration = int(diVideoSet["fDurationAvg"]), bOpticalFlow = False)
+				print("\nCaptured video: %.1f sec, %s, %.1f fps" % \
+					(fElapsed, str(arFrames.shape), len(arFrames)/fElapsed))
 
-			# show orange wait box
-			frame_show(oStream, "orange", "Translating sign ...", tuRectangle = (h, w))
+				# show orange wait box
+				frame_show(oStream, "orange", "Translating sign ...", tuRectangle = (h, w))
 
-			# crop and downsample frames
-			arFrames = images_crop(arFrames, h, w)
-			arFrames = frames_downsample(arFrames, diVideoSet["nFramesNorm"])
+				# crop and downsample frames
+				arFrames = images_crop(arFrames, h, w)
+				arFrames = frames_downsample(arFrames, diVideoSet["nFramesNorm"])
 
-			# Translate frames to flows - these are already scaled between [-1.0, 1.0]
-			print("Calculate optical flow on %d frames ..." % len(arFrames))
-			timer.start()
-			arFlows = frames2flows(arFrames, bThirdChannel = False, bShow = True)
-			print("Optical flow per frame: %.3f" % (timer.stop() / len(arFrames)))
+				# Translate frames to flows - these are already scaled between [-1.0, 1.0]
+				print("Calculate optical flow on %d frames ..." % len(arFrames))
+				timer.start()
+				arFlows = frames2flows(arFrames, bThirdChannel = False, bShow = True)
+				print("Optical flow per frame: %.3f" % (timer.stop() / len(arFrames)))
 
-			# predict video from flows			
-			print("Predict video with %s ..." % (keI3D.name))
-			arX = np.expand_dims(arFlows, axis=0)
-			arProbas = keI3D.predict(arX, verbose = 1)[0]
-			nLabel, sLabel, fProba = probability2label(arProbas, oClasses, nTop = 3)
+				# predict video from flows			
+				print("Predict video with %s ..." % (keI3D.name))
+				arX = np.expand_dims(arFlows, axis=0)
+				arProbas = keI3D.predict(arX, verbose = 1)[0]
 
-			sResults = "Sign: %s (%.0f%%)" % (sLabel, fProba*100.)
-			print(sResults)
-			nCount += 1
+				arTopLabels = arProbas.argsort()[-nTop:][::-1]
+				arTopProbas = arProbas[arTopLabels]
+				for i in range(nTop):
+					print("Top %d: [%3d] %s (confidence %.1f%%)" % \
+						(i+1, arTopLabels[i], label[arTopLabels[i]] + " ", arTopProbas[i]*100.))
 
-		# quit
-		elif key == ord('q'):
-			break
+				nLabel, sLabel, fProba = arTopLabels[0], label[arTopLabels[0]], arTopProbas[0]
+				sResults = "Sign: %s (%.0f%%)" % (sLabel, fProba*100.)
+				print(sResults)
 
-	# do a bit of cleanup
-	oStream.release()
-	cv2.destroyAllWindows()
 
-	return
+				#nLabel, sLabel, fProba = probability2label(arProbas, oClasses, nTop = 3)
+				nCount += 1
+
+			# quit
+			elif key == ord('q'):
+				break
+
+		# do a bit of cleanup
+		oStream.release()
+		cv2.destroyAllWindows()
+
+		return
 
 
 if __name__ == '__main__':
